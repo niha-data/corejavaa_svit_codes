114b267eb24e09c2f523a01628a099102392d0f6
Niharika <niharikaba19@gmail.com>
2025-08-24 19:41:20 +0530

    first commit

---
+++ core_java_svit/src/org/tnsif/sf/c2tc/ScannerClassDemo/ScannerClassDemo.java
@@ -0,0 +1,25 @@
+package org.tnsif.sf.c2tc.ScannerClassDemo;
+
+//import scanner class
+//create scanner object
+//prompt the user input
+//read input using scanner method
+//validate
+//close the scanner
+
+import java.util.Scanner;
+public class ScannerClassDemo {
+
+	public static void main(String[] args) {
+		Scanner scan = new Scanner(System.in);
+		System.out.println("Enter your name");
+		String name = scan.nextLine();
+		System.out.println(name);
+		System.out.println("Enter your age");
+		int age = scan.nextInt();
+		System.out.println(age);
+		scan.close();
+		
+	}
+
+}
